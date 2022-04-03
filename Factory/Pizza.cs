c4be9d818be570dc5d25af2dfac3989dc68374bb
Mona <monasallam400@gmail.com>
2022-04-03 05:44:25 +0200

    Factory

---
+++ Factory/Pizza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Pizza
+    {
+        public void prepare()
+        {
+            Console.WriteLine("prepare");
+        }
+        public void bake()
+        {
+            Console.WriteLine("bake");
+        }
+        public void cut()
+        {
+            Console.WriteLine("cut");
+        }
+        public void box()
+        {
+            Console.WriteLine("box");
+        }
+
+        Pizza orderPizza(String type)
+        {
+            Pizza pizza = null;
+    
+            pizza.prepare();
+            pizza.bake();
+            pizza.cut();
+            pizza.box();
+            return pizza;
+        }
+
+
+    }
+  
+    
+    }
---
+++ Factory/PizzaStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class PizzaStore
+    {
+        SimplePizzaFactory factory;
+
+        public PizzaStore(SimplePizzaFactory factory)
+        {
+            this.factory = factory;
+        }
+        public Pizza orderPizza(String type)
+        {
+            Pizza pizza;
+            pizza = factory.createPizza(type);
+            return pizza;
+        }
+    }
+}
